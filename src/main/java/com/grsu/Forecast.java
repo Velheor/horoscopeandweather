e989a633410c687550e1e857c592c30cb0bb7527
“Vlad <vlad.atsman@gmail.com>
2020-03-05 00:08:40 +0300

    add a new level of abstract

---
+++ src/main/java/com/grsu/Forecast.java
@@ -0,0 +1,27 @@
+package com.grsu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Forecast {
+    private Period period = new Period();
+    private Predict predict = new Predict();
+    private Map<String, String> weatherPredicts = new HashMap<String, String>();
+
+    public String createForecast(String predictType, String dateType) {
+        String predict = this.predict.createPredict(predictType);
+        String period = this.period.createPeriod(dateType);
+        if (predictType.equals("2")) {
+            predict = checkForUniqueWeatherPredict(period, predict);
+        } else
+            predict = this.predict.createPredict(predictType);
+        return period + " " + predict;
+    }
+
+    public String checkForUniqueWeatherPredict(String period, String predict) {
+        if (!weatherPredicts.containsKey(period)) {
+            weatherPredicts.put(period, predict);
+        }
+        return weatherPredicts.get(period);
+    }
+}
--- src/main/java/com/grsu/Menu.java
+++ src/main/java/com/grsu/Menu.java
@@ -1,33 +1,20 @@
 package com.grsu;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Menu {
     private InputOutput inputOutput = new IOConsole();
-    private Period period = new Period();
-    private Predict predict = new Predict();
-    private Map<String, String> weatherPredicts = new HashMap<String, String>();
+    private Forecast forecast = new Forecast();
 
     public void mainMenu() {
-        String periodForPredict = "";
-        String predictForPeriod = "";
         inputOutput.writeMessage("Hello!");
         serviceType();
         String type = inputOutput.getAnswer();
         dateToPredictMenu();
         String date = inputOutput.getAnswer();
         if (date.equals("4")) {
-            inputOutput.writeMessage("Enter date MM/DD/YEAR:");
-            periodForPredict = period.predictForSpecialDate(inputOutput.getAnswer());
-        } else {
-            periodForPredict = period.createPeriod(date);
+            inputOutput.writeMessage("MM.dd.yyyy");
+            date = inputOutput.getAnswer();
         }
-        if (type.equals("2")) {
-            predictForPeriod = checkForUniqueWeatherPredict(periodForPredict, predict.createPredict("2"));
-        } else
-            periodForPredict = predict.createPredict(type);
-        inputOutput.writeMessage(periodForPredict + predictForPeriod);
+        inputOutput.writeMessage(forecast.createForecast(type, date));
         mainMenu();
     }
 
@@ -44,10 +31,5 @@ public class Menu {
         inputOutput.writeMessage("4 - special day");
     }
 
-    public String checkForUniqueWeatherPredict(String period, String predict) {
-        if (!weatherPredicts.containsKey(period)) {
-            weatherPredicts.put(period, predict);
-        }
-        return weatherPredicts.get(period);
-    }
+
 }
